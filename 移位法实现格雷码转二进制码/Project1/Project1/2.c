2400a9a4c04937b3fc95876f6245e961ff3a3856
李江佳俊 <870969909@qq.com>
2019-06-08 18:16:29 +0800

    6_8

---
+++ 移位法实现格雷码转二进制码/Project1/Project1/2.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+//char fnGotnum()
+//{
+//	char R=12;
+//	char C;
+//	C = R;	
+//	while (R )
+//	{
+//		R = R >> 1;
+//		C = C^R;
+//	}
+//	return C;
+//}
+//
+//void print(int i)
+//{
+//	if (i)
+//	{
+//		print(i / 2);
+//		printf("%d", i%2);
+//	}
+//}
+//
+//
+//
+//int main()
+//{
+//	
+//	int i;
+//	i=fnGotnum();
+//	print(i);
+//	system("pause");
+//	return 0;
+//}
