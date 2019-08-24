a8ab67c053ede365ab98ca3f4e61b52b0d40bb78
李江佳俊 <870969909@qq.com>
2019-08-24 14:52:00 +0800

    8_24

---
+++ 手动实现字符串处理函数/Pr/源.cpp
@@ -0,0 +1,34 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#include <stdlib.h>
+//int strncmp(const char *string1, const char *string2, size_t count)
+//{
+//	while (*string1 == *string2 &&count!=0)
+//	{
+//		string1++;
+//		string2++;
+//		count--;
+//	}
+//	if (count == 0)
+//	{
+//		return 0;
+//	}
+//	if (*string1 > *string2)
+//	{
+//		return 1;
+//	}
+//	if (*string1 < *string2)
+//	{
+//		return -1;
+//	}
+//	
+//}
+//int main()
+//{
+//	char* str1 = "bxcvdfascas";
+//	char* str2 = "bxcv";
+//	int ret = strncmp(str1, str2, 4);
+//	printf("%d ", ret);
+//	system("pause");
+//	return 0;
+//}
