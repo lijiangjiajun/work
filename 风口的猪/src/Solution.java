566e1e75525038a5580671e1dcceaca1340b7982
李江佳俊 <870969909@qq.com>
2020-04-12 18:29:31 +0800

    4_12

---
+++ 风口的猪/src/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+
+//时间复杂度和空间复杂度都是最低的算法
+    public int calculateMax(int[] prices) {
+        int firstBuy = Integer.MIN_VALUE, firstSell = 0;
+        int secondBuy = Integer.MIN_VALUE, secondSell = 0;
+
+        for (int curPrice : prices) {
+            firstBuy = Math.max(firstBuy, -curPrice);
+            firstSell = Math.max(firstSell, firstBuy + curPrice);
+            secondBuy = Math.max(secondBuy,firstSell - curPrice);
+            secondSell = Math.max(secondSell, secondBuy + curPrice);
+        }
+        return secondSell;
+    }
+}
