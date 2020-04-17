7a28df903c3e7fa647a1f883c17bc3d2528e256d
李江佳俊 <870969909@qq.com>
2020-04-17 17:17:21 +0800

    4_17

---
+++ 添加一个字母判断字符串是否回文/src/x.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class x {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String str = in.next();
+            System.out.println(Helper(str));
+        }
+    }
+    public static String Helper(String str){
+        int len = str.length();
+        int i = len/2;
+        if(len%2 == 0){
+            if((str.substring(0,i).contains(new StringBuffer(str.substring(i+1)).reverse().toString())) ||
+                    str.substring(i).contains(new StringBuffer(str.substring(0,i-1)).reverse().toString())){
+                return "YES";
+            }
+            return "NO";
+        }else{
+            if((str.substring(0,i+1).contains(new StringBuffer(str.substring(i+1)).reverse().toString())) ||
+                    str.substring(i).contains(new StringBuffer(str.substring(0,i)).reverse().toString())){
+                return "YES";
+            }
+            return "NO";
+        }
+    }
+}
